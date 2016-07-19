90485b770605b8e9fe98a015d0bdee75eb2e7aff
J.T. Earl <jtearl188@gmail.com>
2016-07-19 19:36:17 -0400

    Deleted RedBlackTree class and added one RBNode constructor

--- src/redblacktree/RedBlackTree.java
+++ src/redblacktree/RedBlackTree.java
@@ -21,85 +21,16 @@ class RBNode {
 		this.color = color;
 	}
 
+	public RBNode(int key, RBNode parent, RBNode left, RBNode right, boolean color){
+		this.key = key;
+		this.parent = parent;
+		this.left = left;
+		this.right = right;
+		this.color = color;
+	}
+
 }
 
 class RedBlackTree {
-	private static final boolean RED = true;
-	private static final boolean BLACK = false;
-	private static RBNode root;
-
-	public static void main(String[] args) {
-		RBNode node = new RBNode(10, RED);
-
-	}
-
-	/**
-	 * is node red? False if null
-	 * 
-	 * @param node
-	 * @return
-	 */
-	private static boolean isRed(RBNode node) {
-		if (node == null) {
-			return false;
-		}
-		return node.color == RED;
-	}
-
-	public static void rBInsert(int key) {
-		root = rBInsert(root, key);
-		root.color = BLACK;
-	}
-
-	// insert the node into the subtree rooted at node
-	private static RBNode rBInsert(RBNode node, int key) {
-		if (node == null) {
-			return new RBNode(key, RED);
-		}
-		if (key < node.key) {
-			node.left = rBInsert(node.left, key);
-		} else if (key > node.key) {
-			node.right = rBInsert(node.right, key);
-		}
-		if (isRed(node.right) && !isRed(node.left))
-			node = leftRotate(node);
-		if (isRed(node.left) && isRed(node.left.left))
-			node = rightRotate(node);
-		if (isRed(node.left) && isRed(node.right))
-			flipColors(node);
-		return node;
-	}
-
-	/**
-	 * pivots around the link from node to node2. it makes node2 the new root of
-	 * the subtree with node as node2's left child and node2's left child as
-	 * node's right child. Right child of node can not be null. Runs in O(1)
-	 * time.
-	 * 
-	 * @param t
-	 * @param node
-	 */
-	private static RBNode leftRotate(RBNode node) {
-		RBNode x = node.left;
-		node.left = x.right;
-		x.right = node;
-		x.color = x.right.color;
-		x.right.color = RED;
-		return x;
-	}
-
-	private static RBNode rightRotate(RBNode node) {
-		RBNode x = node.left;
-		node.left = x.right;
-		x.right = node;
-		x.color = x.right.color;
-		x.right.color = RED;
-		return x;
-	}
-
-	private static void flipColors(RBNode node) {
-		node.color = !node.color;
-		node.left.color = !node.left.color;
-		node.right.color = !node.right.color;
-	}
+	
 }
