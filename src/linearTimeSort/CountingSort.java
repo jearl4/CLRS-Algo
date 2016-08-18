ce71db563478e65ec9f15be0161c73d799ead9bf
J.T. Earl <jtearl188@gmail.com>
2016-08-18 16:27:51 -0400

    Fixed bug that caused out of bounds error on line 41

--- src/linearTimeSort/CountingSort.java
+++ src/linearTimeSort/CountingSort.java
@@ -3,9 +3,9 @@ package linearTimeSort;
 import java.util.Arrays;
 
 /**
- * When k equals n the sort runs in O(n) time, or O(k + n). It beats the lower bound of (n lg
- * n) due to the lack of comparisons and departure from the comparison sort
- * model.
+ * When k equals n the sort runs in O(n) time, or O(k + n). It beats the lower
+ * bound of (n lg n) due to the lack of comparisons and departure from the
+ * comparison sort model.
  * 
  * Counting sort is also stable, meaning that which ever numbers appear first in
  * the input come first in the output array. This allows satellite data to be
@@ -20,7 +20,7 @@ public class CountingSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] input = { 5, 4, 3, 2, 1, 0 };
-		int[] sortedArray = new int[6];
+		int[] sortedArray = new int[input.length];
 		countingSort(input, sortedArray, 6);
 		System.out.println(Arrays.toString(sortedArray));
 
@@ -28,7 +28,7 @@ public class CountingSort {
 
 	public static void countingSort(int[] inputArray, int[] sortedArray, int k) {
 		int[] temp = new int[k];
-		for (int i = 0; i < k; i++) {
+		for (int i = 0; i < temp.length; i++) {
 			temp[i] = 0;
 		}
 		for (int j = 0; j < inputArray.length; j++) {
@@ -37,8 +37,8 @@ public class CountingSort {
 		for (int i = 1; i < k; i++) {
 			temp[i] = temp[i] + temp[i - 1];
 		}
-		for (int j = inputArray.length - 1; j > 0; j--) {
-			sortedArray[temp[inputArray[j]]] = inputArray[j];
+		for (int j = sortedArray.length - 1; j >= 0; j--) {
+			sortedArray[temp[inputArray[j]] - 1] = inputArray[j];
 			temp[inputArray[j]] = temp[inputArray[j]] - 1;
 		}
 	}
